0ab4af785e29484ba9ea13cc3456531322bcb700
Eman Munawar <munawareman074@gmail.com>
2026-06-26 02:17:29 -0700

    feat(arrays-strings): add brute force solution for Summary Ranges

---
+++ arrays-strings/brute/SummaryRangesBrute.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SummaryRangesBrute {
+    public static void main(String[] args) {
+        int[] arr = {0,1,2,4,5,6,7,9,10};
+        System.out.println("Array whose range is to find is:" + Arrays.toString(arr));
+        System.out.println("Range of Array: " + summaryRanges(arr));
+
+    }
+    // Function to find summary ranges
+    public static ArrayList<String> summaryRanges(int[] arr) {
+        ArrayList<String> result = new ArrayList<>(); // array list to store final result of ranges
+        int i = 0;
+        while (i < arr.length) {
+            int first = arr[i];
+            int last = first;
+
+            while (i + 1 < arr.length && arr[i + 1] == arr[i] + 1) { // // Check consecutive numbers
+                last = arr[i + 1];
+                i++;
+
+            }
+            if (first == last) {   // For Single number
+                result.add(String.valueOf(first));
+            }
+            else {
+                result.add(first + "->" + last);  // Printing range
+
+            }
+            i++;
+        }
+        return result;
+
+    }
+}
