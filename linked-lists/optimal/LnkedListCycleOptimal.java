a2424dad88dbcbdb798c6d4f3fa6c645a0fe115c
Eman Munawar <munawareman074@gmail.com>
2026-06-26 22:24:26 -0700

    feat(linkedlist): add optimal solution of LinkedListCycle

--- linked-lists/optimal/LnkedListCycleOptimal.java
+++ linked-lists/optimal/LnkedListCycleOptimal.java
@@ -1,17 +1,17 @@
 public class LnkedListCycleOptimal {
-    static class Node{
+    static class ListNode{
         int data;
-        Node next;
-        Node(int data){     // constructor
+        ListNode next;
+        ListNode(int data){     // constructor
             this.data = data;
         }
     }
     public static void main(String[] args){
-        Node head = new Node(2);
-        head.next = new Node(4);
-        head.next.next = new Node(7);
-        head.next.next.next = new Node(2);
-        head.next.next.next.next = new Node(4);
+        ListNode head = new ListNode(2);
+        head.next = new ListNode(4);
+        head.next.next = new ListNode(7);
+        head.next.next.next = new ListNode(2);
+        head.next.next.next.next = new ListNode(4);
         head.next.next.next.next.next = head.next;
         System.out.println("Cycle:" + hasCycle(head));
         System.out.println();
@@ -20,9 +20,9 @@ public class LnkedListCycleOptimal {
         System.out.println("Space Complexity = O(1)");
         System.out.println("===============================");
     }
-    public static boolean hasCycle(Node head){
-        Node slow = head;
-        Node fast = head;
+    public static boolean hasCycle(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
         while(fast!=null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
